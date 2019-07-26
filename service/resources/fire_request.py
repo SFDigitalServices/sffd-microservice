29068c03d4dfa572630745cdb40ee9a8cf771839
Josh Chou <626661+josh-chou@users.noreply.github.com>
2019-07-26 16:49:07 -0700

    convert instance variables to static ones

--- service/resources/fire_request.py
+++ service/resources/fire_request.py
@@ -3,20 +3,20 @@ import os
 import requests
 
 class FireRequest():
-    def __init__(self):
-        proxy_token = os.environ.get('DS_PROXY_TOKEN')
-        api_key = os.environ.get('FIRE_API_KEY')
+    proxy_token = os.environ.get('DS_PROXY_TOKEN')
+    api_key = os.environ.get('FIRE_API_KEY')
 
-        self.requests = requests.Session()
-        self.requests.headers.update({
-            'X-DS-PROXY-TOKEN': proxy_token,
-            'api_key': api_key
-        })
+    reqs = requests.Session()
+    reqs.headers.update({
+        'X-DS-PROXY-TOKEN': proxy_token,
+        'api_key': api_key
+    })
 
-        self.url = os.environ.get('FIRE_DB_URL')
+    url = os.environ.get('FIRE_DB_URL')
 
-    def get(self):
-        return self.requests.get(self.url)
 
-    def post(self, jsonParams):
-        return self.requests.post(self.url, json=jsonParams)
+    def get():
+        return FireRequest.reqs.get(FireRequest.url)
+
+    def post(jsonParams):
+        return FireRequest.reqs.post(FireRequest.url, json=jsonParams)
