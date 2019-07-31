f4f709ef7c19cc5c214d81f994733d31e2004022
Josh Chou <626661+josh-chou@users.noreply.github.com>
2019-07-31 14:06:49 -0700

    pylint and code review changes

--- service/resources/fire_request.py
+++ service/resources/fire_request.py
@@ -1,8 +1,9 @@
-"""subclass of requests"""
+"""FireRequests"""
 import os
 import requests
 
 class FireRequest():
+    """Like requests, but encapsulates api_key and proxy token"""
     proxy_token = os.environ.get('DS_PROXY_TOKEN')
     api_key = os.environ.get('FIRE_API_KEY')
     url = os.environ.get('FIRE_DB_URL')
@@ -12,8 +13,12 @@ class FireRequest():
         'api_key': api_key
     }
 
+    @staticmethod
     def get():
+        """perform get request"""
         return requests.get(FireRequest.url, headers=FireRequest.headers)
 
-    def post(jsonParams):
-        return requests.post(FireRequest.url, headers=FireRequest.headers, json=jsonParams)
+    @staticmethod
+    def post(json_params):
+        """perform post request"""
+        return requests.post(FireRequest.url, headers=FireRequest.headers, json=json_params)
--- service/resources/records.py
+++ service/resources/records.py
@@ -1,39 +1,40 @@
 """Records in the fire db"""
 import json
+from http.client import responses
 import falcon
 import jsend
-from http.client import responses
-from .fire_request import *
-
-from pprint import pprint
+from .fire_request import FireRequest
 
 class Records():
+    """Records Controller"""
     ERROR_MSG = 'There was a problem communicating with the fired db api'
-    
+
     def on_get(self, req, resp):
+        """handle the get request"""
         response = FireRequest.get()
         if response.status_code == 200:
             resp.body = json.dumps(jsend.success(response.json()))
         else:
             resp.body = json.dumps(jsend.error(Records.ERROR_MSG))
             resp.status = str(response.status_code) + " " + responses[response.status_code]
-        
+
     def on_post(self, req, resp):
-        apiParams = {
+        """handle the post request"""
+        api_params = {
             'revision': None
         }
-        apiParams.update(req.params)
+        api_params.update(req.params)
 
-        response = FireRequest.post(json=apiParams)
+        response = FireRequest.post(api_params)
 
         if response and response.status_code == 200:
-            id = response.headers.get('id', False)
+            return_id = response.headers.get('id', False)
             payload = {
                 'message':response.headers.get('result_message')
             }
-            if id:
+            if return_id:
                 # response returned an id
-                payload['id'] = id
+                payload['id'] = return_id
                 resp.body = json.dumps(jsend.success(payload))
                 resp.status = falcon.HTTP_200
             else:
